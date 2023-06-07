f65167bc797ca954766b2dc611f43510ba95844a
Brooke Adlesperger-Jones <badles@live.com>
2023-06-07 17:20:56 -0500

    added main menu function

--- index.js
+++ index.js
@@ -15,3 +15,56 @@ db.connect((err) => {
 });
 
 module.exports = db;
+
+function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          name: 'choice',
+          type: 'list',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+          ],
+        },
+      ])
+      .then((answer) => {
+        switch (answer.choice) {
+          case 'View all departments':
+            // Call the function to view all departments
+            break;
+          case 'View all roles':
+            // Call the function to view all roles
+            break;
+          case 'View all employees':
+            // Call the function to view all employees
+            break;
+          case 'Add a department':
+            // Call the function to add a department
+            break;
+          case 'Add a role':
+            // Call the function to add a role
+            break;
+          case 'Add an employee':
+            // Call the function to add an employee
+            break;
+          case 'Update an employee role':
+            // Call the function to update an employee role
+            break;
+          case 'Exit':
+            connection.end(); // Close the database connection
+            return;
+        }
+      });
+  }
+  
+  // Call the mainMenu function to start the application
+  mainMenu();
+  
